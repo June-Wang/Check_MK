eadacbd63937eac3959a5d87fde53ce44ee6328e
Alex Wang <qq5910225@gmail.com>
2016-06-27 17:40:20 +0800

    修改路径

--- notifications/modify_notifications.sh
+++ notifications/modify_notifications.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -i 's/LONGDATETIME/SHORTDATETIME/g' /usr/share/check_mk/notifications/mail
+#sed -i 's/LONGDATETIME/SHORTDATETIME/g' /usr/share/check_mk/notifications/mail
+find /omd/ -type f -name "mail"|xargs -r -i sed -i 's/LONGDATETIME/SHORTDATETIME/g' '{}'
