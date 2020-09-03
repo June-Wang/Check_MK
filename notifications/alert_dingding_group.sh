c918866386c5c3387d9e8785145affc24dfb7a6a
xiaojun.wang <xiaojun.wang@advance.ai>
2020-09-03 14:14:04 +0800

    fix

--- notifications/alert_dingding_group.sh
+++ notifications/alert_dingding_group.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #Send msg by dingding Group
 
-token_url='https://oapi.dingtalk.com/robot/send?access_token=XXX'
 export LANG=en_US.UTF-8
 
-location='阿里云'
+location='阿里云BJ'
 
-PRINTSERV="${location}
-$NOTIFY_HOSTALIAS $NOTIFY_SERVICEDESC is $NOTIFY_SERVICESTATE
-IPAddr: $NOTIFY_HOSTADDRESS
-Date: $NOTIFY_SHORTDATETIME
-Message: $NOTIFY_SERVICEOUTPUT\n$NOTIFY_LONGSERVICEOUTPUT"
+PRINTSERV="${location} $NOTIFY_HOSTALIAS $NOTIFY_HOSTADDRESS $NOTIFY_SERVICEDESC is $NOTIFY_SERVICESTATE
 
-PRINTHOST="${location}
-$NOTIFY_HOSTALIAS is $NOTIFY_HOSTSTATE
-IPAddr:$NOTIFY_HOSTADDRESS
-Date: $NOTIFY_SHORTDATETIME
-Message: $NOTIFY_HOSTOUTPUT"
+$NOTIFY_SERVICEOUTPUT\n$NOTIFY_LONGSERVICEOUTPUT
+日期: $NOTIFY_SHORTDATETIME 
+地址: $NOTIFY_HOSTADDRESS"
+
+PRINTHOST="${location} $NOTIFY_HOSTALIAS $NOTIFY_HOSTADDRESS is $NOTIFY_HOSTSTATE
+
+$NOTIFY_HOSTOUTPUT
+日期: $NOTIFY_SHORTDATETIME
+地址: $NOTIFY_HOSTADDRESS"
 
 if [ "$NOTIFY_WHAT" == "SERVICE" ]; then
     # For Service notification
@@ -26,6 +25,7 @@ else
     msg=${PRINTHOST}
 fi
 
+token_url='https://oapi.dingtalk.com/robot/send?access_token=ea723ca5419a7f3084aa7b4c103aa0160a105385a3f9af9a5d5994f992876d6a'
 curl "${token_url}" \
    -H 'Content-Type: application/json' \
    -d "{'msgtype': 'text', 
